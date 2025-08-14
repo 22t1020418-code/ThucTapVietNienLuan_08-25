700cc9575aa2d85f971a45f40b3622281df03e1d
22t1020418-code <22t1020418@husc.edu.vn>
2025-08-14 20:19:42 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,16 @@
 <?php
-$host = "dpg-d24qm3ili9vc73ej472g-a";
+$host = "dpg-d2ett6juibrs738epejg-a.singapore-postgres.render.com";
 $port = "5432";
-$dbname = "db_quanlythuchi";
-$user = "db_quanlythuchi_user";
-$password = "lMMkElD7noEARgTdAdH7l1nj8EictGsi";
+$dbname = "db_quanlythuchi_dey5";
+$user = "db_quanlythuchi_dey5_user";
+$password = "rt0ya0aedHgvQj8Ww3wi57U34HOqe5P8";
 
-// Kết nối PostgreSQL
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+// Kết nối PostgreSQL qua SSL
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
 
 // Kiểm tra kết nối
 if (!$conn)
     die("Kết nối đến PostgreSQL thất bại.");
 ?>
+
+
